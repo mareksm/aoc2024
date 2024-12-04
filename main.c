79f4da7b9e562f12b459ca7be4cb779ebbb9d226
mareksm <46475431+mareksm@users.noreply.github.com>
2024-12-04 17:30:40 +0200

    Add files via upload

--- main.c
+++ main.c
@@ -1,5 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+
+#define array_len(a) ((&a)[1] - a)
 
 int compare(const void *a, const void *b)
 {
@@ -24,8 +27,58 @@ int count_occurances(int *arr, int size, int target)
     return count;
 }
 
+int numbers(char *line, int *v, int arrsz)
+{
+    char *p = line, *e;
+    int c = 0;
+
+    while (*p)
+    {
+        v[c] = strtol(p, &e, 10);
+        if (p == e)
+            break;
+        p = e;
+        c++;
+        if (c > arrsz)
+            return 0;
+    }
+    return c;
+}
+
+void print_arr(int *v, int ln)
+{
+    printf("%d: ", ln);
+    for (int i = 0; i < ln; i++)
+        printf("%d ", v[i]);
+    printf("\n");
+}
+
+int unsafe(int *v, int arrsz)
+{
+    if (arrsz < 2)
+        return -1;
+
+    int order = v[0] - v[1];
+
+    if (order == 0)
+        return 0;
+
+    for (int i = 1; i < arrsz; i++)
+    {
+        int curr_order = v[i - 1] - v[i];
+        if (curr_order == 0 || (order > 0 && curr_order < 0) || (order < 0 && curr_order > 0) || abs(curr_order) > 3)
+        {
+            return i - 1;
+        }
+    }
+
+    return -1;
+}
+
 int main(int argc, char **argv)
 {
+    /* day 1 */
+
 #define arr_len 1000
     int left[arr_len], right[arr_len];
     int i = 0;
@@ -53,6 +106,57 @@ int main(int argc, char **argv)
         day_1_b += left[i] * count_occurances(right, arr_len, left[i]);
 
     printf("DAY 1 (b): %d\n", day_1_b);
+
+    /* day 2 */
+
+    char line[256];
+    int safe_level = 0;
+
+    file = fopen("input_d2.txt", "r");
+    while (fgets(line, sizeof(line), file))
+    {
+        int v[9], c = numbers(line, v, array_len(v));
+
+        if (unsafe(v, c) == -1)
+            safe_level++;
+    }
+    fclose(file);
+
+    printf("DAY 2 (a): %d\n", safe_level);
+
+    safe_level = 0;
+    file = fopen("input_d2.txt", "r");
+    while (fgets(line, sizeof(line), file))
+    {
+        int v[9], c = numbers(line, v, array_len(v));
+
+        int uidx = unsafe(v, c);
+        if (uidx != -1)
+        {
+
+            int m = 0;
+            do
+            {
+                int vd[c - 1], j = 0;
+                for (int i = 0; i < c; i++)
+                {
+                    if (i != m)
+                        vd[j++] = v[i];
+                }
+                if (unsafe(vd, c - 1) == -1)
+                {
+                    safe_level++;
+                    break;
+                }
+
+            } while (++m < c);
+        }
+        else
+            safe_level++;
+    }
+    fclose(file);
+
+    printf("DAY 2 (b): %d\n", safe_level);
 
     return 0;
 }
